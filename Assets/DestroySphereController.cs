3bdeca1d4044f81548cd21adcb6fb376d0117ff0
GT20176071\user <nkc.shunta.kawai@gmail.com>
2017-10-26 16:49:23 +0900

    完成

--- Assets/DestroySphereController.cs
+++ Assets/DestroySphereController.cs
@@ -1,26 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DestroySphereController : MonoBehaviour {
+public class DestroySphereController : MonoBehaviour
+{
+
     public void Shot(Vector3 dir)
     {
         GetComponent<Rigidbody>().AddForce(dir);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision other)
     {
         GetComponent<Rigidbody>();
-        
+        if (other.gameObject.tag == "Sphere")
+        {
+            Destroy(other.gameObject);
+        }
     }
-
+    
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         //Shot(new Vector3(0, 200, 2000));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.y < -5)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
+
