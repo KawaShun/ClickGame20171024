3c5620c1d235440409f57f5668b2728fab52ef5b
GT20176071\user <nkc.shunta.kawai@gmail.com>
2017-10-26 10:40:44 +0900

    玉を飛ばす

---
+++ Assets/DestroySphereController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySphereController : MonoBehaviour {
+    public void Shot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>();
+        
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        //Shot(new Vector3(0, 200, 2000));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/DestroySphereGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySphereGenerator : MonoBehaviour {
+
+    public GameObject DestroySpherePrefab;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject DestroySphere = 
+                Instantiate(DestroySpherePrefab) as GameObject;
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            DestroySphere.GetComponent<DestroySphereController>().Shot(worldDir.normalized * 2000);
+        }
+    }
+}
